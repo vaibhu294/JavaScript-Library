62244296eb100d5b4ca4b7003b40995cc39dbb32
vaibhu <107000845+vaibhu294@users.noreply.github.com>
2023-12-20 18:09:35 +0000

    added new variable file

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,15 @@
+const account_Id = 13594
+let account_Email = "vaibhav01@google.com"
+var account_Password = "125423";
+account_City = "Jaipur";
+let account_State;
+
+// account_Id = 2
+
+account_Email = "abcds@gmail.com";
+account_Password = "987654";
+account_City = "Pune";
+
+console.log(account_Id);
+
+console.table([account_Id, account_Email, account_Password, account_City, account_State]);
